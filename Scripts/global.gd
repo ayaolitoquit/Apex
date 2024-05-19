d0e7e7b9a7e01b2507789418e72d741684ef5329
@jmlll <123856684+jmlll-afk@users.noreply.github.com>
2024-05-19 21:45:50 +0800

    committttt

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,3 +1,7 @@
 extends Node
 
 var max_speed = 150
+var array = ["+50","-20", "Better luck next time"]
+var rng = RandomNumberGenerator.new()
+var buff = rng.randi_range (0, 2)
+
---
+++ scenes/PowerUp.gd
@@ -0,0 +1,70 @@
+extends CanvasLayer
+
+var timer = Timer.new()
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	self.visible = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_egg_body_entered(body):
+	if Global.buff == 0:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+		
+	elif Global.buff == 1:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	elif Global.buff == 2:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	else:
+		self.visible = false
+	
+	self.visibile = false
+
+
+func _on_egg_2_body_entered(body):
+	if Global.buff == 0:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	elif Global.buff == 1:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	elif Global.buff == 2:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	else:
+		self.visible = false
+		
+	self.visible = false
+
+
+func _on_egg_3_body_entered(body):
+	if Global.buff == 0:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	elif Global.buff == 1:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	elif Global.buff == 2:
+		$PowerUp.text = str(Global.array[Global.buff])
+		print(Global.array[Global.buff])
+		self.visible = true
+	else:
+		self.visible = false
+	
+	self.visible = false
