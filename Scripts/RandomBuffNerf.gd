ef18e8f8be5232cca59a030758da3a7668f29917
Keezgeez <135505653+Keezgeez@users.noreply.github.com>
2024-05-19 20:04:06 +0800

    Randomizer

--- Scripts/RandomBuffNerf.gd
+++ Scripts/RandomBuffNerf.gd
@@ -3,19 +3,17 @@ extends Node2D
 var rng = RandomNumberGenerator.new()
 var array = ["+50","-20", "Better luck next time"]
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	rng.seed = 1
+	rng.seed = 10
 	
-
+func _on_egg_3_body_entered(body):
+	print(rng.randi_range(1,3))
+	
+func _on_egg_2_body_entered(body):
+	print(rng.randi_range(1,3))
 
 func _on_egg_body_entered(body):
 	print(rng.randi_range(1,3))
 
 
-func _on_egg_2_body_entered(body):
-	print(rng.randi_range(1,3))
-
-
-func _on_egg_3_body_entered(body):
-	print(rng.randi_range(1,3))
+	
