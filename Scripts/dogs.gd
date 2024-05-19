bcf2dba93a89789b9dd3cf43979e15347d82fb5c
Keezgeez <135505653+Keezgeez@users.noreply.github.com>
2024-05-19 15:36:19 +0800

    Doggie
    Pikon na ko kayo naman

--- Scripts/dogs.gd
+++ Scripts/dogs.gd
@@ -1,15 +1,6 @@
-extends CharacterBody2D
+extends Node2D
 
-var speed = 100
-var motion = Vector2.ZERO
 var player = null
-
-func _physics_process(delta):
-	motion = Vector2.ZERO
-	if player:
-		motion = position.direction_to(player.position) * speed
-	motion = move_and_slide()
-		
 
 
 func _on_area_2d_body_entered(body):
